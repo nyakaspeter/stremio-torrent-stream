9a7bea1d297eee1cf32d8b4b9a4b2f0c9374c6e2
Péter Nyakas <thenyakaspeter@gmail.com>
2024-01-11 02:20:15 +0100

    add logo

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ const manifest: Stremio.Manifest = {
   resources: ["stream"],
   types: ["movie", "series"],
   name: "Jackett Stream",
+  logo: "https://user-images.githubusercontent.com/27040483/28728094-99f3e3f6-73c7-11e7-8f8d-28912dc6ac0d.png",
+  background:
+    "https://i.etsystatic.com/35367581/r/il/53bf97/4463935832/il_fullxfull.4463935832_3k3g.jpg",
   description:
     "This addon can enable Stremio to stream movies and series from torrents returned by a Jackett API. To make it work you'll need to set up torrent trackers in Jackett using it's Web UI and run a Torrent stream server.",
   idPrefixes: ["tt"],
